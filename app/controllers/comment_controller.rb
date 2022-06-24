782d68a5b3e143041a74f094cef9d356f1a2c887
Ahsan Amin <amin.ahsan2@gmail.com>
2022-06-24 11:29:50 +0500

    CRUD-comments completed

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -5,13 +5,37 @@ class CommentController < ApplicationController
     end
 
     def create
-        data = params.require(:comment).permit(:text)
-        @product = current_user.products.new(:text: data[:text], prod_id: )
+        # byebug
+        data = params.require(:comment).permit(:text, :product_id)
+        # byebug
+        @comment = current_user.comments.new({:text => params["comment"]["name"], :product_id => params["product_id"]})
 
-        if @product.save
-            redirect_to product_own_path
+        if @comment.save
+            redirect_to product_path(params["product_id"])
         else
             redirect_to new_product_path
         end
     end
+
+
+    def edit
+    end
+
+    def update
+        temp = params.require(:comment).permit(:text)
+        @comment = Comment.find(params[:id])
+        
+        if @comment.update(temp)
+            redirect_to product_path(params[:product_id])
+        else
+            redirect_to edit_product_comment_path(params[:product_id], params[:id])
+        end
+    end
+
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy()
+        redirect_to product_path(params[:product_id])
+    end
+
 end
